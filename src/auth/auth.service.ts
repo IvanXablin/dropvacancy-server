60e8c56832660c6bf5201eef1fdda3a9448a7ffe
Ivan Kondratev <amdand67890@gmail.com>
2023-02-10 18:56:10 +0700

    jwt auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,16 +4,27 @@ import { ModelType } from "@typegoose/typegoose/lib/types";
 import { hash, genSalt, compare } from "bcryptjs";
 import { UserModel } from "../user/user.model";
 import { AuthDto } from "./dto/auth.dto";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>) {}
+  constructor(
+    @InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>,
+    private readonly jwtService: JwtService
+  ) {}
 
-  async login(dto: AuthDto):Promise<UserModel>  {
-    return this.validateUser(dto)
+  async login(dto: AuthDto) {
+    const user = await this.validateUser(dto);
+
+    const tokens = await this.issueTokenPair(String(user._id));
+
+    return {
+      user: this.getUserFields(user),
+      ...tokens
+    };
   }
 
-  async register(dto: AuthDto):Promise<UserModel>  {
+  async register(dto: AuthDto) {
     const oldUser = await this.UserModel.findOne({ email: dto.email });
 
     if (oldUser) throw new BadRequestException('User with this email is already in the system!');
@@ -26,11 +37,18 @@ export class AuthService {
       password: await hash(dto.password, salt)
     });
 
-    return newUser.save();
+    const user = await newUser.save();
+
+    const tokens = await this.issueTokenPair(String(user._id));
+
+    return {
+     user: this.getUserFields(user),
+      ...tokens
+    }
   }
 
-  async validateUser(dto: AuthDto):Promise<UserModel> {
-    const user = await this.UserModel.findOne({ email: dto.email });
+  async validateUser(dto: AuthDto) {
+    const user = await this.UserModel.findOne({ name: dto.name, email: dto.email });
 
     if (!user) throw new UnauthorizedException('User not found!');
 
@@ -39,5 +57,27 @@ export class AuthService {
     if (!isValidPassword) throw new UnauthorizedException('Invalid Password!');
 
     return user;
+  }
+
+  async issueTokenPair(userId: string) {
+    const data = { _id: userId };
+
+    const refreshToken = await this.jwtService.signAsync(data, {
+      expiresIn: '14d'
+    });
+
+    const accessToken = await this.jwtService.signAsync(data, {
+      expiresIn: '1h'
+    });
+
+    return { refreshToken, accessToken };
+  }
+
+  getUserFields(user: UserModel) {
+    return {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    };
   }
 }
