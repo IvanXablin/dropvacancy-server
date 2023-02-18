6ae6a921878a36be6f79ece99a75438472149664
Ivan Kondratev <you@example.com>
2023-02-18 20:42:22 +0700

    docker-compose

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
